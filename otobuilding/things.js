3977dc8078c10a6f331526a5e9dc27e0aff2b0da
verygois <sorryforthedelayinsending.vg.pe.hu>
2023-12-10 15:07:01 +0900

    heard

--- otobuilding/things.js
+++ otobuilding/things.js
@@ -56,8 +56,11 @@ let otobuilAll = {
       },
       properties: {
         title: '小泉さんにもろたマスクやろ？',
-        date: '2020年6月20日頃',
-        address: '住之江区北加賀屋２丁目３−１５'
+        date: '2020年6月20日 13:30頃',
+        address: '住之江区北加賀屋２丁目３−１５',
+        link: {
+          html: 'otobuilding/date/shiko.html'
+        }
       }
     },
     {
@@ -109,7 +112,10 @@ let otobuilAll = {
       properties: {
         title: 'ゆずの匂いすごいやろ。ごめんな',
         date: '2021年12月22日 23:30',
-        address: '西成区橘３丁目１２−２０'
+        address: '西成区橘３丁目１２−２０',
+        link: {
+          html: 'otobuilding/date/asahiyu.html'
+        }
       }
     },
     {
@@ -133,7 +139,10 @@ let otobuilAll = {
       properties: {
         title: '「クリスマスに何もらった？」<br/>「ヘッドホン」',
         date: '2021年12月31日 17:00頃',
-        address: '大正区南恩加島６丁目５−５'
+        address: '大正区南恩加島６丁目５−５',
+        link: {
+          html: 'otobuilding/date/yupopo.html'
+        }
       }
     },
     {
@@ -157,7 +166,10 @@ let otobuilAll = {
       properties: {
         title: 'ブッ刺したろか',
         date: '2022年2月5日 19:00頃',
-        address: '住之江区御崎２丁目５−１'
+        address: '住之江区御崎２丁目５−１',
+        link: {
+          html: 'otobuilding/date/himematsuonsen.html'
+        }
       }
     },
     {
@@ -338,6 +350,244 @@ let otobuilAll = {
         address: '住之江区御崎3-1-17',
         link: {
           html: 'otobuilding/date/20220225.html'
+        }
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.4827294191162, 34.60191716499787]
+      },
+      properties: {
+        title: 'なぁ、ほんまのこと言うて？',
+        date: '2022年2月8日 16:25',
+        address: '住之江区西住之江３丁目３'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.4808284868539, 34.61827594571754]
+      },
+      properties: {
+        title: 'すばらしいやないか！',
+        date: '2022年2月8日 19:30',
+        address: '住之江区中加賀屋３丁目７'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.49187281851079, 34.618000679283696]
+      },
+      properties: {
+        title: '中途半端なことがいちばん困んねやぁ',
+        date: '2022年2月13日 19:15',
+        address: '住吉区東粉浜３丁目２３'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.49227968288875, 34.605819819360846]
+      },
+      properties: {
+        title: '私、初恋時代は絶対忘れへん！一生！',
+        date: '2022年2月27日 21:30',
+        address: '住吉区清水丘１丁目３１−１５',
+        link: {
+          html: 'otobuilding/date/hamaguchionsen.html'
+        }
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.47290133878147, 34.60749330611684]
+      },
+      properties: {
+        title: '偉かったね、もう終わったよ。<br/>よくがんばったね〜',
+        date: '2022年3月17日 16:50',
+        address: '住之江区新北島１丁目２−１',
+        link: {
+          html: 'otobuilding/date/20220317.html'
+        }
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.49603212288486, 34.64744739591383]
+      },
+      properties: {
+        title: 'くっせー服',
+        date: '2022年3月26日 16:10',
+        address: '西成区鶴見橋１丁目１７−１５'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.48934698606456, 34.614762993711466]
+      },
+      properties: {
+        title: 'あかんあかん、あかんてだから〜',
+        date: '2022年2月13日 19:30',
+        address: '住之江区粉浜３丁目10-18'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.48806306208354, 34.61672899645829]
+      },
+      properties: {
+        title: 'たえちゃんが背中を押してくれたから今日は耐えれた',
+        date: '2022年2月16日 20:20',
+        address: '住之江区粉浜西２丁目１９'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.48573910200872, 34.61480869515299]
+      },
+      properties: {
+        title: 'お風呂掃除したら50円な？',
+        date: '2022年2月23日 10:15',
+        address: '住之江区粉浜西３丁目４'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.50391050613052, 34.64546789712162]
+      },
+      properties: {
+        title: 'やるぞ ‼',
+        date: '2022年2月26日 13:44',
+        address: '西成区太子２丁目３−１０'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.4796960670957, 34.614665866352524]
+      },
+      properties: {
+        title: '走れクソガキ',
+        date: '2022年2月26日 19:10',
+        address: '住之江区中加賀屋４丁目６−２８'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.48994910185849, 34.60640581645879]
+      },
+      properties: {
+        title: 'SHE’S 住吉',
+        date: '2022年2月27日 22:30',
+        address: '住之江区安立２丁目２'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.48683721540584, 34.61318000271726]
+      },
+      properties: {
+        title: 'りおちゃんはどうしてそんなに可愛いの？',
+        date: '2022年3月1日 19:40',
+        address: '住之江区浜口西１丁目１'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.47666421864864, 34.60006459036512]
+      },
+      properties: {
+        title: '大和川ヘルスストリート',
+        date: '2022年3月5日 16:00',
+        address: '住之江区北島３丁目１６'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.4686466499881, 34.61044215073433]
+      },
+      properties: {
+        title: '極楽っちゅうもんや',
+        date: '2022年3月5日 16:50',
+        address: '住之江区泉１丁目１−８２'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.476455672234, 34.62205989666809]
+      },
+      properties: {
+        title: 'おいしかったぁ、死ぬおえ吐く〜',
+        date: '2022年3月12日 18:50',
+        address: '住之江区北加賀屋５丁目１−１'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.47776778334372, 34.620788703512616]
+      },
+      properties: {
+        title: '雨最高〜 雨やっば 雨さいっこう！',
+        date: '2022年3月19日 16:00',
+        address: '住之江区西加賀屋１−１−４１'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.49133472122134, 34.62641776667715]
+      },
+      properties: {
+        title: 'keep your emotion',
+        date: '2022年3月26日 14:30',
+        address: '西成区玉出中1-12-13'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.47306292634534, 34.62458544610712]
+      },
+      properties: {
+        title: '春はなんかおかしいですわ',
+        date: '2022年3月25日 11:30頃',
+        address: '住之江区北加賀屋５丁目５−１',
+        link: {
+          html: 'otobuilding/date/20220325.html',
+          youtube: "j2ADyQyHfdY"
         }
       }
     },
@@ -632,241 +882,6 @@ let otobuilAll = {
       type: 'Feature',
       geometry: {
         type: 'Point',
-        coordinates: [135.4827294191162, 34.60191716499787]
-      },
-      properties: {
-        title: 'なぁ、ほんまのこと言うて？',
-        date: '2022年2月8日 16:25',
-        address: '住之江区西住之江３丁目３'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.4808284868539, 34.61827594571754]
-      },
-      properties: {
-        title: 'すばらしいやないか！',
-        date: '2022年2月8日 19:30',
-        address: '住之江区中加賀屋３丁目７'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.49187281851079, 34.618000679283696]
-      },
-      properties: {
-        title: '中途半端なことがいちばん困んねやぁ',
-        date: '2022年2月13日 19:15',
-        address: '住吉区東粉浜３丁目２３'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.49227968288875, 34.605819819360846]
-      },
-      properties: {
-        title: '私、初恋時代は絶対忘れへん！一生！',
-        date: '2022年2月27日 21:30',
-        address: '住吉区清水丘１丁目３１−１５'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.47290133878147, 34.60749330611684]
-      },
-      properties: {
-        title: '偉かったね、もう終わったよ。<br/>よくがんばったね〜',
-        date: '2022年3月17日 16:50',
-        address: '住之江区新北島１丁目２−１',
-        link: {
-          html: 'otobuilding/date/20220317.html'
-        }
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.49603212288486, 34.64744739591383]
-      },
-      properties: {
-        title: 'くっせー服',
-        date: '2022年3月26日 16:10',
-        address: '西成区鶴見橋１丁目１７−１５'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.48934698606456, 34.614762993711466]
-      },
-      properties: {
-        title: 'あかんあかん、あかんてだから〜',
-        date: '2022年2月13日 19:30',
-        address: '住之江区粉浜３丁目10-18'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.48806306208354, 34.61672899645829]
-      },
-      properties: {
-        title: 'たえちゃんが背中を押してくれたから今日は耐えれた',
-        date: '2022年2月16日 20:20',
-        address: '住之江区粉浜西２丁目１９'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.48573910200872, 34.61480869515299]
-      },
-      properties: {
-        title: 'お風呂掃除したら50円な？',
-        date: '2022年2月23日 10:15',
-        address: '住之江区粉浜西３丁目４'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.50391050613052, 34.64546789712162]
-      },
-      properties: {
-        title: 'やるぞ ‼',
-        date: '2022年2月26日 13:44',
-        address: '西成区太子２丁目３−１０'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.4796960670957, 34.614665866352524]
-      },
-      properties: {
-        title: '走れクソガキ',
-        date: '2022年2月26日 19:10',
-        address: '住之江区中加賀屋４丁目６−２８'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.48994910185849, 34.60640581645879]
-      },
-      properties: {
-        title: 'SHE’S 住吉',
-        date: '2022年2月27日 22:30',
-        address: '住之江区安立２丁目２'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.48683721540584, 34.61318000271726]
-      },
-      properties: {
-        title: 'りおちゃんはどうしてそんなに可愛いの？',
-        date: '2022年3月1日 19:40',
-        address: '住之江区浜口西１丁目１'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.47666421864864, 34.60006459036512]
-      },
-      properties: {
-        title: '大和川ヘルスストリート',
-        date: '2022年3月5日 16:00',
-        address: '住之江区北島３丁目１６'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.4686466499881, 34.61044215073433]
-      },
-      properties: {
-        title: '極楽っちゅうもんや',
-        date: '2022年3月5日 16:50',
-        address: '住之江区泉１丁目１−８２'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.476455672234, 34.62205989666809]
-      },
-      properties: {
-        title: 'おいしかったぁ、死ぬおえ吐く〜',
-        date: '2022年3月12日 18:50',
-        address: '住之江区北加賀屋５丁目１−１'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.47776778334372, 34.620788703512616]
-      },
-      properties: {
-        title: '雨最高〜 雨やっば 雨さいっこう！',
-        date: '2022年3月19日 16:00',
-        address: '住之江区西加賀屋１−１−４１'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.49133472122134, 34.62641776667715]
-      },
-      properties: {
-        title: 'keep your emotion',
-        date: '2022年3月26日 14:30',
-        address: '西成区玉出中1-12-13'
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [135.47306292634534, 34.62458544610712]
-      },
-      properties: {
-        title: '春はなんかおかしいですわ',
-        date: '2022年3月25日 11:30頃',
-        address: '住之江区北加賀屋５丁目５−１',
-        link: {
-          html: 'otobuilding/date/20220325.html',
-          youtube: "j2ADyQyHfdY"
-        }
-      }
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
         coordinates: [135.48680727044314, 34.635939323277086]
       },
       properties: {
@@ -1032,7 +1047,10 @@ let otobuilAll = {
       properties: {
         title: 'すごいな私',
         date: '2022年5月22日 18:18',
-        address: '住之江区粉浜西３丁目１−８'
+        address: '住之江区粉浜西３丁目１−８',
+        link: {
+          html: 'otobuilding/date/ur-sumiyoshi.html'
+        }
       }
     }
   ]
