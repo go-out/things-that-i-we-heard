b60651ededfc7c1c34a7e091680cc74f38c245ef
∧° ┐ <we.are.pe.hu@gmail.com>
2023-12-12 17:05:04 +0900

    heard

--- osaka-nantou/things.js
+++ osaka-nantou/things.js
@@ -23,6 +23,42 @@ let osakaNantou = {
         date: '2022年6月19日 21:35',
         address: '大阪市東住吉区つるが丘温'
       }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.50592420684535, 34.651366241594744]
+      },
+      properties: {
+        title: 'ワンランク上の串カツ',
+        date: '2022年11月23日 17:25',
+        address: '大正五年創業 創作串カツ 串揚げ ぎふや別邸 大阪新世界店'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.51974776637041, 34.597437761470644]
+      },
+      properties: {
+        title: '17時までにお風呂に入った方全員にアイスコーヒーまたはアイスクリームをプレゼント！',
+        date: '2023年3月21日 16:50',
+        address: '大阪市住吉区苅田8-10-27 錦温泉'
+      }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.5094782, 34.5913334]
+      },
+      properties: {
+        title: '獅子が空を翔ぶ日に',
+        date: '2023年3月21日 18:45',
+        address: '大阪市住吉区浅香１丁目６'
+      }
     }
   ]
 };
