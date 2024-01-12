f75dd6d7aee740d33e13ad3faac1263def6ebab3
verygois <sorryforthedelayinsending.vg.pe.hu>
2024-01-13 02:06:31 +0900

    heard

---
+++ www/js/csvtojson.js
@@ -0,0 +1,23 @@
+'use strict'
+
+async function csvtojson(csv) {
+    const response = await fetch(csv);
+    const text = await response.text();
+    const data = text.trim().split('\n')
+        .map(line => line.split(',').map(x => x.trim()))
+        .map(marker => {
+            let thisMarker = {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [marker[1], marker[2]]
+                },
+                'properties': {
+                    'title': marker[4].replace(/"/g, ''),
+                    'date': marker[0],
+                    'address': marker[3].replace(/"/g, '')
+                }
+            };
+            things.features.push(thisMarker);
+        });
+};
