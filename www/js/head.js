5bd3222893511e3614a7348c66726a0742e7944c
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-02-01 17:05:25 +0900

    bna

--- www/js/head.js
+++ www/js/head.js
@@ -1,5 +1,21 @@
 'use strict'
 
+async function fetchHTML(url = '', query = '') {
+    fetch(url)
+        .then(response => response.text())
+        .then(innerHTML => {
+            document.querySelector(query).innerHTML = innerHTML;
+        });
+}
+
+async function fetchText(url = '', query = '') {
+    fetch(url)
+        .then(response => response.text())
+        .then(innerText => {
+            document.querySelector(query).innerText = innerText;
+        });
+}
+
 switch (document.readyState) {
     case "loading":
         // この文書はまだ読み込み中
@@ -38,17 +54,33 @@ switch (document.readyState) {
         // この文書は読み込みが終了した。 DOM 要素にアクセスできるようになった。
         // しかし、画像、スタイルシート、フレームなどの副リソースはまだ読み込み中。
 
-        for (const jsEach of indexThis.things) {
-            const script = document.createElement("script")
-            script.src = jsEach.js;
+        const title = document.querySelector('#title')
+        title.addEventListener('click', function () {
+            document.body.classList.toggle('enter');
+        }, false)
 
+        const mainBtn = document.querySelector('#changeHidden')
+        mainBtn.addEventListener('click', function () {
+            document.body.classList.add('enter');
+            const mainAll = document.querySelectorAll('main');
+            mainAll.forEach(main => {
+                if (main.hidden == false) {
+                    main.hidden = true;
+                    mainBtn.textContent = "?";
+                } else {
+                    main.hidden = false;
+                    mainBtn.textContent = "×";
+                }
+            })
+        }, false);
+
+        for (const jsEach of indexThis.things) {
             let heardArr = jsEach.arr;
             let pushArr = jsEach.push;
-            for (const pushArr of heardArr.heard) {
-                things.features.push(pushArr);
-            }
 
-            head.appendChild(script)
+            heardArr.heard.forEach((thisAll)
+                => things.features.push(thisAll)
+            )
         }
 
         break;
@@ -57,5 +89,60 @@ switch (document.readyState) {
         // ページが完全に読み込み完了。
 
         let allThings = things.features[length];
+        for (const marker of things.features) {
+            const pin = document.createElement('div');
+            pin.className = 'thing';
+            new mapboxgl.Marker(pin)
+                .setLngLat(marker.geometry.coordinates)
+                .setPopup(
+                    new mapboxgl.Popup({
+                        offset: 12.5
+                    })
+                        .setHTML(`
+                        <h3>${marker.properties.title}</h3>
+                        <p class="date">
+                        ${marker.properties.date}</br>
+                        ${marker.properties.address}
+                        </p>
+                        `
+                        )
+                )
+                .addTo(map)
+
+            pin.addEventListener('click', () => {
+                // JSON に link オブジェクトがある場合
+                if (marker.properties.link) {
+                    const mainBtn = document.querySelector('#changeHidden');
+                    const mainAll = document.querySelectorAll('main');
+
+                    mainAll.forEach(main => {
+                        if (main.hidden == false) {
+                            main.hidden = true;
+                            mainBtn.textContent = "?";
+                            mainBtn.classList.replace('pause', 'play');
+                        } else {
+                            main.hidden = false;
+                            mainBtn.textContent = "×";
+                            mainBtn.classList.replace('play', 'pause');
+                        }
+                    })
+
+                    if (marker.properties.link.html) {
+                        let html = directory + marker.properties.link.html;
+                        fetchHTML(html, '#about article');
+                    }
+                }
+
+                flyToMarker(marker)
+            })
+        }
+
+        function flyToMarker(e) {
+            map.flyTo({
+                center: e.geometry.coordinates,
+                essential: true,
+                zoom: indexThis.zoomIn
+            });
+        }
         break;
 }
