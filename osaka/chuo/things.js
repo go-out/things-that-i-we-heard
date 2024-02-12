20ef759df0680910715e821a30c3eaf6f4d4df00
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-02-13 00:11:23 +0900

    Update things.js

--- osaka/chuo/things.js
+++ osaka/chuo/things.js
@@ -11,6 +11,18 @@ let osaka4th = {
         date: '2024年1月21日 14:00',
         address: '大阪市浪速区浪速区元町2-9-19難波八阪神社'
       }
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [135.49761391755527, 34.65512385982274]
+      },
+      properties: {
+        title: '京都新聞テレビ広告<br>東京マラソン',
+        date: '2024年2月12日 9:45',
+        address: '大阪市浪速区大国1丁目1番14'
+      }
     }
   ]
 };
