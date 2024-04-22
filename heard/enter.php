84c08cf401ce977084d018065594229d3c890217
verygois <sasajimakazuma@gmail.com>
2024-04-23 00:05:39 +0900

    heard

--- heard/enter.php
+++ heard/enter.php
@@ -6,14 +6,14 @@
     $hqdn = $_SERVER["REMOTE_PORT"];
     $os = $_SERVER["HTTP_USER_AGENT"];
 
-    echo "IP <b id='ip'>" . $ip . "</b><br/>";
-    echo "PORT <b id='hqdn'>" . $hqdn . "</b><br/>";
-    echo "<small id='os'>" . $os . "</small>";
+    echo "IP <b id='ip'>" . $ip . "</b> | ";
+    echo "PORT <b id='hqdn'>" . $hqdn . "</b>";
+    echo "<p><u id='os'>" . $os . "</u></p>";
     ?>
     <p>
         下記の Come on join us ! を クリックすると、このウェブサイトに
         あなたの通信情報／ブラウザ等情報 が 送信され、あなたが聞いたこと
         を 追加・記録 できるようになります。
     </p>
+    <button type="button" onclick="setLOG()">Come on join us !</button>
 </div>
-<button type="button" onclick="setLOG()">Come on join us !</button>
