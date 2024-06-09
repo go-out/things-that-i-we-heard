a2a6453c5203a66a86042deb4896b6cec642c766
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-06-09 10:35:27 +0900

    Update gifu.js

--- japan/gifu.js
+++ japan/gifu.js
@@ -4,6 +4,198 @@ let gifuHeard = {
             type: 'Feature',
             geometry: {
                 type: 'Point',
+                coordinates: [137.25762697804805,36.142763981818405]
+            },
+            properties: {
+                title: 'お前、計算も出来んのか',
+                date: '2024年5月27日 8:51',
+                address: '岐阜県高山市本町2-52 Cafe Don'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25716443944395,36.14091905289938]
+            },
+            properties: {
+                title: '飛騨へ飛ばすぞ、なんか言いよってね',
+                date: '2024年5月18日 16:20',
+                address: '岐阜県高山市有楽町72'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25916973369414,36.14118526540764]
+            },
+            properties: {
+                title: 'おすすめどぉれぇ？インスタ映えするのどぉれ',
+                date: '2024年5月18日 15:49',
+                address: '岐阜県高山市片原町75 バグパイプ'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25875740940415,36.14168932440015]
+            },
+            properties: {
+                title: '米に炭入れて炊いたらうまいらしいぞ',
+                date: '2024年5月18日 8:24',
+                address: '岐阜県高山市片原町12'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25875164782366,36.14170269810792]
+            },
+            properties: {
+                title: '今日は被った人多いな',
+                date: '2024年5月18日  8:19',
+                address: '岐阜県高山市片原町12'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25825504790248,36.13964415647551]
+            },
+            properties: {
+                title: 'フライパンで煎って三つほど弾けたら使ってください',
+                date: '2024年5月18日 8:03',
+                address: '岐阜県高山市八軒町1-5 高山陣屋'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.18065928662736,36.24060242053589]
+            },
+            properties: {
+                title: 'ただいま..え〜...300円引きつけちゃいました',
+                date: '2024年5月11日 15:34',
+                address: '岐阜県飛騨市古川町栄2丁目8番2'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25762056160602,36.142760431155104]
+            },
+            properties: {
+                title: 'サササドル..拭いときましたので雨が降ってましたので',
+                date: '2024年5月12日 16:07',
+                address: '岐阜県高山市本町2-52 Cafe Don'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.2573545112245,36.14855937529725]
+            },
+            properties: {
+                title: 'ありがとっっ',
+                date: '2024年5月12日 12:06',
+                address: '岐阜県高山市大新町2丁目22'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25766453634378,36.1427547099805]
+            },
+            properties: {
+                title: 'もうめんどくさいな、やっちゃん出て',
+                date: '2024年5月12日 16:04',
+                address: '岐阜県高山市本町2-52 Cafe Don'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.2595310976585,36.14236517480286]
+            },
+            properties: {
+                title: 'すぐ使うんじゃなくてまず飾って楽しむ',
+                date: '2024年5月12日 9:45',
+                address: '岐阜県高山市上三之町80'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.2612856733507,36.14223226084191]
+            },
+            properties: {
+                title: 'not HARAKIRI',
+                date: '2024年5月12日 10:09',
+                address: '岐阜県高山市上一之町75'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.25961433866541,36.142395090107954]
+            },
+            properties: {
+                title: '大切な日はそれぞれ違うのでね',
+                date: '2024年5月12日 9:38',
+                address: '岐阜県高山市上三之町80'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.262307991596,36.13965125204149]
+            },
+            properties: {
+                title: '温度差激しいねぇ',
+                date: '2024年5月11日 9:28',
+                address: '岐阜県高山市堀端町70'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.26201844158214,36.14020851688919]
+            },
+            properties: {
+                title: '新芽やもんで新芽は弱いもんで',
+                date: '2024年5月11日 9:26',
+                address: '岐阜県高山市堀端町60'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [137.26231643368124,36.139643815534015]
+            },
+            properties: {
+                title: 'べーーっしゃり',
+                date: '2024年5月11日 9:23',
+                address: '岐阜県高山市堀端町70'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
                 coordinates: [137.33264534964943, 36.169546970888504]
             },
             properties: {
@@ -57,7 +249,7 @@ let gifuHeard = {
             properties: {
                 title: 'ただ俺誕生日知らんもんで~',
                 date: '2024年4月20日 15:53',
-                address: '岐阜県高山市有楽町45寿美久'
+                address: '岐阜県高山市有楽町45 寿美久'
             }
         }
     ]
