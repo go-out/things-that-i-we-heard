df299c6676ef002c923c9d5dd96cd5d76372e398
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-06-09 10:58:37 +0900

    Update hokusetsu.js

--- osaka/hokusetsu.js
+++ osaka/hokusetsu.js
@@ -59,6 +59,162 @@ let osakaHokusetsu = {
                 date: '2024年4月7日 11:31',
                 address: '大阪府茨木市総持寺1丁目2番11'
             }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.613181313447,34.85214332460501]
+          },
+          properties: {
+            title: 'いっぱいや並んでる',
+            date: '2024年5月5日 16:45',
+            address: '大阪府高槻市芥川町3丁目5番7'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.60523307541575,34.85527653972002]
+          },
+          properties: {
+            title: 'こいのぼりさんバイバーイ',
+            date: '2024年5月5日 16:00',
+            address: '大阪府高槻市門前橋'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.6162784528687,34.855348333821425]
+          },
+          properties: {
+            title: '地中海風ハイツ',
+            date: '2024年5月5日 13:45',
+            address: '大阪府高槻市天神町2丁目1番5'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.61318425522592,34.85249969519204]
+          },
+          properties: {
+            title: 'はよにぎれにぎれ',
+            date: '2024年5月5日 16:50',
+            address: '大阪府高槻市芥川町3-5'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.46718403194473,34.77275460383963]
+          },
+          properties: {
+            title: '絶対ないと思うけど',
+            date: '2024年5月11日 14:20',
+            address: '大阪府豊中市曽根東町1丁目2番7'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.47530754709885,34.77146602249039]
+          },
+          properties: {
+            title: 'どこ行くの？',
+            date: '2024年5月12日 14:50',
+            address: '大阪府豊中市城山町1丁目7番27'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.47710240604,34.77011407080519]
+          },
+          properties: {
+            title: 'みんな来たくて来てるんじゃないから',
+            date: '2024年5月11日 15:45',
+            address: '大阪府豊中市城山町1丁目10番9'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.47204266248212,34.76859038434428]
+          },
+          properties: {
+            title: '浴場にタコの絵あるだけで爆上がり',
+            date: '2024年5月11日 20:10',
+            address: '大阪府豊中市曽根東町6-7-15'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.47980424357172,34.775334065952336]
+          },
+          properties: {
+            title: '只者じゃない学校だなこれは',
+            date: '2024年5月11日 16:00',
+            address: '大阪府豊中市長興寺南4丁目2番15'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.4793486676001,34.769278998438935]
+          },
+          properties: {
+            title: '選ばれしものしか住めない街',
+            date: '2024年5月11日 15:45',
+            address: '大阪府豊中市城山町4丁目7番16'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.48425147602916,34.77618883631682]
+          },
+          properties: {
+            title: '自由 天国',
+            date: '2024年5月11日 14:30',
+            address: '大阪府豊中市服部緑地1 山ヶ池'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [135.48022707220423,34.771175068594914]
+          },
+          properties: {
+            title: '天竺ロード',
+            date: '2024年5月11日 16:10',
+            address: '大阪府豊中市服部緑地 新蒟蒻橋'
+          }
+        },
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [___]
+          },
+          properties: {
+            title: '___',
+            date: '2024年5月__日 ___',
+            address: '___'
+          }
         }
     ]
 };
